ad3ad612741471bcb56b854b3a12b2a4a0d115fb
Isaac Asante <W0725009@myscc.ca>
2020-11-10 14:11:04 -0500

    Main basis of all screens done
    Made the basis of all dashboard, budget, and transaction for app with widgets.

---
+++ Overture/BudgetViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BudgetViewController.swift
+//  Overture
+//
+//  Created by Isaac Asante on 2020-11-09.
+//
+
+import Foundation
+import UIKit
+import Charts
+
+class BudgetViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+        
+        
+    }
---
+++ Overture/TransactionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TransactionViewController.swift
+//  Overture
+//
+//  Created by Isaac Asante on 2020-11-09.
+//
+
+import Foundation
+import UIKit
+
+
+class TransactionViewController: UIViewController {
+    
+}
